5413eca254773eb54be9e1f38b14d8de5a0c6037
Maxim Khatskevich <maxim@khatskevi.ch>
2019-05-07 20:51:39 +0200

    [ADD] app setup script from ‘RepoConfigurator’ examples
    plus minimal necessary (empty) unit tests implementation to make unit tests build and pass out of the box.
    See more at https://github.com/XCEssentials/RepoConfigurator

---
+++ .setup/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:4.2
+// Managed by ice
+
+import PackageDescription
+
+let package = Package(
+    name: "AppTemplateSetup",
+    dependencies: [
+        .package(url: "https://github.com/nvzqz/FileKit", from: "5.2.0"),
+        .package(url: "https://github.com/XCEssentials/RepoConfigurator", from: "2.2.0")
+    ],
+    targets: [
+        .target(
+            name: "AppTemplateSetup",
+            dependencies: ["XCERepoConfigurator", "FileKit"],
+            path: ".",
+            sources: ["main.swift"]
+        )
+    ]
+)

8af94bbe3d809a0495d1723261f523d29d9faffd
Maxim Khatskevich <maxim@khatskevi.ch>
2020-05-10 16:21:30 +0200

    [UPGRADE] to ‘RepoConfigurator’ v.3.0
    plus minor updates of other depenecies.

--- .setup/Package.swift
+++ .setup/Package.swift
@@ -1,13 +1,15 @@
-// swift-tools-version:4.2
-// Managed by ice
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "AppTemplateSetup",
+    platforms: [
+        .macOS(.v10_11),
+    ],
     dependencies: [
         .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
-        .package(url: "https://github.com/XCEssentials/RepoConfigurator", from: "2.7.0")
+        .package(url: "https://github.com/XCEssentials/RepoConfigurator", from: "3.0.0")
     ],
     targets: [
         .target(
